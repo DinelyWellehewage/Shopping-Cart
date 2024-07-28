b5f5faebe23b5ea21d162e0e1cafc64e143e7723
DinelyWellehewage <dinelywh78@gmail.com>
2024-07-28 22:17:37 +0200

    handling the form state

--- src/Products/ProductForm.tsx
+++ src/Products/ProductForm.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Product } from "./products.slice";
+import { title } from "process";
 
-interface ProductFormProps {}
+const ProductForm: React.FC = ({}) => {
+  const [{ title, price, id }, setProduct] = useState<Product>({
+    id: "",
+    title: "",
+    price: 0,
+  });
 
-const ProductForm: React.FC<ProductFormProps> = ({}) => {
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct((prev) => {
+      (prev as any)[name] = value;
+      const newValue = { ...prev };
+      return newValue;
+    });
+  };
   return (
     <>
       <h2>Add Game to the Store</h2>
       <form>
-        <input type="text" placeholder="Game Title" name="title" />
-        <input type="number" placeholder="Price" name="price" />
-        <input type="text" placeholder="Id" name="id" />
+        <input
+          type="text"
+          placeholder="Game Title"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          name="price"
+          value={price}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Id"
+          name="id"
+          value={id}
+          onChange={handleChange}
+        />
         <button>Add Price</button>
       </form>
     </>
